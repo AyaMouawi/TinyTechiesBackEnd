0df07d781d636bb326f49b82030d5bb04e552ebb
AyaMouawi <ayamouawi8@gmail.com>
2023-10-17 12:45:45 +0300

    session

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,48 +16,70 @@ const getAllUsers = async (req, res) =>{ // Name the function according to the t
         });
     }};
 
-    const getUserByID = async (req, res) => {
+    const getUserByID = async (req, res) => { // naming change
         try {
-          const [result] = await db.query(`SELECT * FROM users WHERE User_id = ?`, [
+          const [result] = await db.query(`SELECT * FROM users WHERE User_id = ?`, [ // the query change
             req.params.id,
           ]);
           res.status(200).json({
             success: true,
-            message: 'User data retrieved successfully',
+            message: 'data retrieved successfully',
             data: result,
           });
         } catch (error) {
           res.status(400).json({
             success: false,
-            message: 'Unable to add new user',
+            message: 'Unable to get data',
             error,
           });
         }
       };
 
-      const deleteUser= async (req, res) => {
+      const deleteUser= async (req, res) => { //naming change 
         try {
-          const [result] = await db.query(`DELETE * FROM users WHERE User_id = ?`, [
+          const [result] = await db.query(`DELETE FROM users WHERE User_id = ?`, [ // the query changes 
             req.params.id,
           ]);
           res.status(200).json({
             success: true,
-            message: 'User data retrieved successfully',
+            message: 'data deleted successfully',
             data: result,
           });
         } catch (error) {
           res.status(400).json({
             success: false,
-            message: 'Unable to add new user',
+            message: 'Unable to delete data',
             error,
           });
         }
       };
 
+    const addUser = async (req, res) => {
+        const {UserFullName , Password , UserEmail , UserAge , UserAbsence, Role} = req.body;  // change according to the fields of the table you are working on 
+        try {
+            const result = await db.query ('INSERT INTO users (UserFullName , Password , UserEmail , UserAge , UserAbsence, Role) VALUES (?,?,?,?,?,?);',// change according to the fields of the table you are working on 
+            [UserFullName , Password , UserEmail , UserAge , UserAbsence, Role]); // change according to the fields of the table you are working on 
+
+            res.status(201).json({
+                success: true,
+                message: 'User added successfully',
+              });
+            } catch (error) {
+              res.status(400).json({
+                success: false,
+                message: 'Unable to add new user',
+                error,
+              });
+            }  
+        };
+
+    
+     
 
 
 
 
 
 
-    module.exports = { getAllUsers, getUserByID, deleteUser};  // export the functions names you wrote
+
+    module.exports = { getAllUsers, getUserByID, deleteUser, addUser};  // export the functions names you wrote
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,10 +5,12 @@ const { // you get the functions name you wrote in the coresponding controller
     getAllUsers, 
     getUserByID,
     deleteUser,
+    addUser,
 } = require('../controllers/userController'); // the controller change according to the folder name example :'../controllers/projectController'
 
 router.get('/getAll' , getAllUsers); // put the function name corresponding to what you wrote in the controller
 router.get('/get/:id' , getUserByID);
-router.delete('/delete/:id' , deleteUser)
+router.delete('/delete/:id' , deleteUser);
+router.post('/add', addUser);
 
 module.exports  = router;
